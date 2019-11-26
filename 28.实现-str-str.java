10dd8101bfded100eeddc68c8ee94e1033cf4e08
freesinger <wzy545@vip.qq.com>
2019-11-27 02:44:32 +0800

    Update

---
+++ 28.实现-str-str.java
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=java
+ *
+ * [28] 实现 strStr()
+ *
+ * https://leetcode-cn.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (39.02%)
+ * Likes:    304
+ * Dislikes: 0
+ * Total Accepted:    102.7K
+ * Total Submissions: 262.9K
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * 实现 strStr() 函数。
+ * 
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置
+ * (从0开始)。如果不存在，则返回  -1。
+ * 
+ * 示例 1:
+ * 
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 
+ * 
+ * 说明:
+ * 
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * 
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+
+        /**
+         * KMP
+         * @reference: https://leetcode-cn.com/problems/implement-strstr/solution/shen-ru-qian-chu-kmp-suan-fa-yu-ping-jie-by-zohary/
+         */
+        // int strLen = haystack.length(), subLen = needle.length();
+        // if (subLen == 0) return 0;
+        // if (strLen == 0) return -1;
+        // // 构建状态机
+        // int[][] FSM = new int[subLen][256];
+        // int X = 0, match = 0;
+        // for (int i = 0; i < subLen; i++) {
+        //     match = (int) needle.charAt(i);
+        //     for (int j = 0; j < 256; j++) {
+        //         // 当前状态 + 匹配失败字符 = 孪生词缀状态 + 匹配字符
+        //         FSM[i][j] = FSM[X][j]; 
+        //     }
+        //     FSM[i][match] = i + 1;
+        //     if (i > 0) {
+        //         // 下一孪生前缀状态 = X + match
+        //         X = FSM[X][match];
+        //     }
+        // }
+        // // 匹配子串
+        // int state = 0;
+        // for (int i = 0; i < strLen; i++) {
+        //     state = FSM[state][haystack.charAt(i)];
+        //     if (state == subLen) {
+        //         return i - subLen + 1;
+        //     }
+        // }
+        // return -1;
+    
+    }
+}
+// @lc code=end
+
---
+++ 35.搜索插入位置.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (44.56%)
+ * Likes:    362
+ * Dislikes: 0
+ * Total Accepted:    92.8K
+ * Total Submissions: 208K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 
+ * 你可以假设数组中无重复元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null) return -1;
+        if (nums.length == 0 || target < nums[0]) return 0;
+        if (target > nums[nums.length-1]) return nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return i;
+            if (nums[i] < target && target < nums[i+1]) return i+1;
+        }
+        
+        return -1;
+    }
+}
+// @lc code=end
+
