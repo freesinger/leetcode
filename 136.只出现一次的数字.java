4635f0aa76b6535fd0c7c833523993075f739366
freesinger <wzy545@vip.qq.com>
2019-12-08 19:55:01 +0800

    ckp

--- 136.只出现一次的数字.java
+++ 136.只出现一次的数字.java
@@ -35,7 +35,11 @@
 // @lc code=start
 class Solution {
     public int singleNumber(int[] nums) {
-        
+        if (nums == null) return Integer.valueOf(null);
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            res = res ^ nums[i];
+        return res;
     }
 }
 // @lc code=end
