bb5c17b964d773065279a3376534683f69244c45
freesinger <wzy545@vip.qq.com>
2020-01-08 00:58:08 +0800

    check

---
+++ 374.猜数字大小.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=java
+ *
+ * [374] 猜数字大小
+ *
+ * https://leetcode-cn.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (41.36%)
+ * Likes:    45
+ * Dislikes: 0
+ * Total Accepted:    14.8K
+ * Total Submissions: 35.2K
+ * Testcase Example:  '10\n6'
+ *
+ * 我们正在玩一个猜数字游戏。 游戏规则如下：
+ * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+ * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+ * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+ * 
+ * -1 : 我的数字比较小
+ * ⁠1 : 我的数字比较大
+ * ⁠0 : 恭喜！你猜对了！
+ * 
+ * 
+ * 示例 :
+ * 
+ * 输入: n = 10, pick = 6
+ * 输出: 6
+ * 
+ */
+
+// @lc code=start
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1, right = n, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (guess(mid) == 0) return mid;
+            else if (guess(mid) > 0) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
---
+++ 383.赎金信.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=383 lang=java
+ *
+ * [383] 赎金信
+ *
+ * https://leetcode-cn.com/problems/ransom-note/description/
+ *
+ * algorithms
+ * Easy (50.61%)
+ * Likes:    63
+ * Dislikes: 0
+ * Total Accepted:    14.4K
+ * Total Submissions: 28K
+ * Testcase Example:  '"a"\n"b"'
+ *
+ * 给定一个赎金信 (ransom)
+ * 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。如果可以构成，返回
+ * true ；否则返回 false。
+ * 
+ * (题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。)
+ * 
+ * 注意：
+ * 
+ * 你可以假设两个字符串均只含有小写字母。
+ * 
+ * 
+ * canConstruct("a", "b") -> false
+ * canConstruct("aa", "ab") -> false
+ * canConstruct("aa", "aab") -> true
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (magazine.contains(ransomNote)) return true;
+        Map<Character, Integer> rmap = new HashMap<>();
+        Map<Character, Integer> mmap = new HashMap<>();
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            Character ch = ransomNote.charAt(i);
+            if (!rmap.containsKey(ch)) {
+                rmap.put(ch, 1);
+            } else {
+                rmap.put(ch, rmap.get(ch)+1);
+            }
+        }
+
+        for (int j = 0; j < magazine.length(); j++) {
+            Character ch = magazine.charAt(j);
+            if (!mmap.containsKey(ch)) {
+                mmap.put(ch, 1);
+            } else {
+                mmap.put(ch, mmap.get(ch)+1);
+            }
+        }
+
+        for (Character ch: rmap.keySet()) {
+            if (mmap.get(ch) == null) return false;
+            if (rmap.get(ch) > mmap.get(ch)) return false;
+        }
+
+        return true;
+    }
+}
+// @lc code=end
+
