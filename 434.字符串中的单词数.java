cd4a40b64c073567868b3abc50a0ca4d43ba1c0a
freesinger <wzy545@vip.qq.com>
2020-01-12 19:09:21 +0800

    check

---
+++ 434.字符串中的单词数.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=434 lang=java
+ *
+ * [434] 字符串中的单词数
+ *
+ * https://leetcode-cn.com/problems/number-of-segments-in-a-string/description/
+ *
+ * algorithms
+ * Easy (32.83%)
+ * Likes:    44
+ * Dislikes: 0
+ * Total Accepted:    11.1K
+ * Total Submissions: 33.2K
+ * Testcase Example:  '"Hello, my name is John"'
+ *
+ * 统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+ * 
+ * 请注意，你可以假定字符串里不包括任何不可打印的字符。
+ * 
+ * 示例:
+ * 
+ * 输入: "Hello, my name is John"
+ * 输出: 5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int countSegments(String s) {
+        String trimmed = s.trim();
+        if (trimmed.equals("")) {
+            return 0;
+        }
+        return trimmed.split("\\s+").length;
+    }
+}
+// @lc code=end
+
