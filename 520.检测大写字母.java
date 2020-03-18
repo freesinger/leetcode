57b8148e11e5bc3fa9bfaca91e9175e8ca47fe7a
freesinger <wzy545@vip.qq.com>
2020-03-18 12:54:56 +0800

    Check

---
+++ 520.检测大写字母.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=520 lang=java
+ *
+ * [520] 检测大写字母
+ *
+ * https://leetcode-cn.com/problems/detect-capital/description/
+ *
+ * algorithms
+ * Easy (53.79%)
+ * Likes:    77
+ * Dislikes: 0
+ * Total Accepted:    16.1K
+ * Total Submissions: 29.4K
+ * Testcase Example:  '"USA"'
+ *
+ * 给定一个单词，你需要判断单词的大写使用是否正确。
+ * 
+ * 我们定义，在以下情况时，单词的大写用法是正确的：
+ * 
+ * 
+ * 全部字母都是大写，比如"USA"。
+ * 单词中所有字母都不是大写，比如"leetcode"。
+ * 如果单词不只含有一个字母，只有首字母大写， 比如 "Google"。
+ * 
+ * 
+ * 否则，我们定义这个单词没有正确使用大写字母。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: "USA"
+ * 输出: True
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: "FlaG"
+ * 输出: False
+ * 
+ * 
+ * 注意: 输入是由大写和小写拉丁字母组成的非空单词。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        // char[] words = word.toCharArray();
+        int upper = 0, lower = 0;
+        // int[] wordNum = new int[2];
+        // Arrays.fill(wordNum, 0);
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isUpperCase(ch)) upper++;
+            if (Character.isLowerCase(ch)) lower++;
+        }
+
+        if (word.length() != upper + lower) return false;
+        if (lower == word.length() || upper == word.length()) return true;
+        if (upper == 1 && Character.isUpperCase(word.charAt(0))) return true;
+
+        return false;
+    }
+}
+// @lc code=end
+
---
+++ 563.二叉树的坡度.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=563 lang=java
+ *
+ * [563] 二叉树的坡度
+ *
+ * https://leetcode-cn.com/problems/binary-tree-tilt/description/
+ *
+ * algorithms
+ * Easy (50.72%)
+ * Likes:    57
+ * Dislikes: 0
+ * Total Accepted:    9.2K
+ * Total Submissions: 17.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个二叉树，计算整个树的坡度。
+ * 
+ * 一个树的节点的坡度定义即为，该节点左子树的结点之和和右子树结点之和的差的绝对值。空结点的的坡度是0。
+ * 
+ * 整个树的坡度就是其所有节点的坡度之和。
+ * 
+ * 示例:
+ * 
+ * 
+ * 输入: 
+ * ⁠        1
+ * ⁠      /   \
+ * ⁠     2     3
+ * 输出: 1
+ * 解释: 
+ * 结点的坡度 2 : 0
+ * 结点的坡度 3 : 0
+ * 结点的坡度 1 : |2-3| = 1
+ * 树的坡度 : 0 + 0 + 1 = 1
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 任何子树的结点的和不会超过32位整数的范围。
+ * 坡度的值不会超过32位整数的范围。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int tiltSum = 0;
+
+    public int findTilt(TreeNode root) {
+        traverse(root);
+        return tiltSum;
+    }
+
+    /**
+     * 递归
+     * 注意返回值是子树和
+     * @param node
+     * @return
+     */
+    private int traverse(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = traverse(node.left);
+        int right = traverse(node.right);
+        tiltSum += Math.abs(left-right);
+
+        return left+right+node.val;
+    }
+    
+    // List<Integer> tilts = new ArrayList<>();
+
+    // public int findTilt(TreeNode root) {
+    //     int tiltSum = 0;
+
+    //     nodeTilt(root);
+    //     for (int t : tilts) tiltSum += t;
+
+    //     return tiltSum;
+    // }
+
+    // private void nodeTilt(TreeNode node) {
+    //     if (node == null || (node.left == null && node.right == null)) return;
+    //     if (node.left == null) {
+    //         tilts.add(Math.abs(childSum(node.right)));
+    //         nodeTilt(node.right);
+    //     } else if (node.right == null) {
+    //         tilts.add(Math.abs(childSum(node.left)));
+    //         nodeTilt(node.left);
+    //     } else {
+    //         tilts.add(Math.abs(childSum(node.left)-childSum(node.right)));
+    //         nodeTilt(node.right);
+    //         nodeTilt(node.left);
+    //     }
+    // }
+
+    // private int childSum(TreeNode node) {
+    //     if (node == null) return 0;
+    //     // if (node.left == null && node.right == null) return node.val;
+    //     return childSum(node.left) + childSum(node.right) + node.val; 
+    // }
+}
+// @lc code=end
+
