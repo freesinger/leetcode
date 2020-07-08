bf137980b683efc6d14e55a50eebf03a2805e15d
freesinger <wzy545@vip.qq.com>
2020-07-08 12:24:44 +0800

    Udpate

--- redis/RedisDemo.java
+++ redis/RedisDemo.java
@@ -1,9 +1,17 @@
 import redis.clients.jedis.Jedis;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RedisDemo {
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("127.0.0.1");
-        System.out.println("Connect to Redis succeed!");
-        System.out.println("Sever is running: " + jedis.ping());
+//        Jedis jedis = new Jedis("127.0.0.1");
+//        System.out.println("Connect to Redis succeed!");
+//        System.out.println("Sever is running: " + jedis.ping());
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer("string"); // add
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.size());
     }
 }
